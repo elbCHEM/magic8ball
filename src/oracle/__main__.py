ea1036d9ad101f31478b4ef40ec2faab030790e1
ELB <elb@chem.ku.dk>
2025-09-09 11:16:57 +0200

    Added support for providing random seed

--- src/oracle/__main__.py
+++ src/oracle/__main__.py
@@ -1,4 +1,15 @@
-from oracle.flow import main
+import click
+import random
+from typing import Optional
+from oracle.flow import main as app
+
+
+@click.command()
+@click.option('--seed', type=click.INT, default=None, help="Random seed")
+def main(seed: Optional[int] = None) -> None:
+    if seed is not None:
+        random.seed(seed)
+    app()
 
 
 if __name__ == '__main__':
